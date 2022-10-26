d8f5325135feecff82aed249c686d4de85437ce8
Vinicius Zaniboni Chagas <Vinicius.ZaniboniChagas-ext@ab-Inbev.com>
2022-10-26 09:52:52 -0300

    Eventos, emitindo eventos, recebendo eventos

---
+++ src/app/components/eventos/eventos.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+export class EventosComponent implements OnInit {
+
+  show: boolean = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showMessage(): void {
+    this.show = !this.show;
+  }
+
+}
